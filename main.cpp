8f570cc32875b040983eab85ae6773aab86f6e5d
Anjan Roy <anjanroy@yandex.com>
2021-10-20 05:36:16 +0000

    added timing; use subgroup local memory caching

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <CL/sycl.hpp>
+#include <chrono>
 #include <iostream>
 
 using namespace sycl;
@@ -6,7 +7,7 @@ using namespace sycl;
 const uint N = 4;
 const uint B = 4;
 
-void condense(queue &q, const float *mat, float *det) {
+int64_t condense(queue &q, const float *mat, float *det) {
   float *mat_ = (float *)malloc(sizeof(float) * N * N);
   float *tmp = (float *)malloc(sizeof(float) * N * N);
   float *arr = (float *)malloc(sizeof(float) * (N - 2));
@@ -18,6 +19,9 @@ void condense(queue &q, const float *mat, float *det) {
   buffer<float, 2> b_mat{mat_, range<2>{N, N}};
   buffer<float, 2> b_tmp{tmp, range<2>{N, N}};
   buffer<float, 1> b_arr{mat, range<1>{N - 2}};
+
+  std::chrono::_V2::steady_clock::time_point start =
+      std::chrono::steady_clock::now();
 
   for (uint i = 0; i < N - 2; i++) {
     int l[1] = {-1};
@@ -43,7 +47,7 @@ void condense(queue &q, const float *mat, float *det) {
 
     if (l[0] == -1) {
       *det = 0;
-      return;
+      return 0;
     }
 
     uint l_idx = l[0];
@@ -53,31 +57,39 @@ void condense(queue &q, const float *mat, float *det) {
           a_mat{b_mat, h, range<2>{N - i, N - i}, id<2>{i, i}};
       accessor<float, 2, access::mode::write, access::target::global_buffer>
           a_tmp{b_tmp, h, range<2>{N - (i + 1), N - (i + 1)},
-                id<2>{i + 1, i + 1}};
+                id<2>{i + 1, i + 1}, noinit};
       accessor<float, 1, access::mode::write, access::target::global_buffer>
-          a_arr{b_arr, h, range<1>{1}, id<1>{i}};
+          a_arr{b_arr, h, range<1>{1}, id<1>{i}, noinit};
+      accessor<float, 1, access::mode::read_write, access::target::local> a_lds{
+          range<1>{1}, h};
 
       h.parallel_for(nd_range<2>{range<2>{N - (i + 1), N - (i + 1)},
                                  range<2>{1, N - (i + 1) > B ? B : 2}},
                      [=](nd_item<2> it) {
+                       ONEAPI::sub_group sg = it.get_sub_group();
+                       if (ONEAPI::leader(sg)) {
+                         a_lds[0] = a_mat[0][l_idx];
+                       }
+
+                       sg.barrier();
+
                        const size_t r = it.get_global_id(0);
                        const size_t c = it.get_global_id(1);
 
                        if (c >= l_idx) {
-                         a_tmp[r][c] = a_mat[0][l_idx] * a_mat[r + 1][c + 1] -
+                         a_tmp[r][c] = a_lds[0] * a_mat[r + 1][c + 1] -
                                        a_mat[0][c + 1] * a_mat[r + 1][l_idx];
                        } else {
-                         a_tmp[r][c] = (0 - a_mat[r + 1][c] * a_mat[0][l_idx]);
+                         a_tmp[r][c] = (0 - a_mat[r + 1][c] * a_lds[0]);
                        }
 
                        if (r == 0 && c == 0) {
-                         a_arr[0] =
-                             sycl::pow(a_mat[0][l_idx], (float)((N - i) - 2));
+                         a_arr[0] = sycl::pow(a_lds[0], (float)((N - i) - 2));
                        }
                      });
     });
 
-    auto evt = q.submit([&](handler &h) {
+    q.submit([&](handler &h) {
       accessor<float, 2, access::mode::write, access::target::global_buffer>
           a_mat{b_mat, h, range<2>{N - (i + 1), N - (i + 1)},
                 id<2>{i + 1, i + 1}};
@@ -87,7 +99,6 @@ void condense(queue &q, const float *mat, float *det) {
 
       h.copy(a_tmp, a_mat);
     });
-    evt.wait();
   }
 
   host_accessor<float, 2, access::mode::read> h_mat{b_mat, range<2>{2, 2},
@@ -102,6 +113,11 @@ void condense(queue &q, const float *mat, float *det) {
   }
 
   *det = lst_det / mult;
+
+  std::chrono::_V2::steady_clock::time_point end =
+      std::chrono::steady_clock::now();
+  return std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
+      .count();
 }
 
 void example_matrix(float *const mat) {
@@ -146,8 +162,9 @@ int main() {
   float det = 0;
   example_matrix(mat);
 
-  condense(q, mat, &det);
-  std::cout << "determinant " << det << std::endl;
+  int64_t ts = condense(q, mat, &det);
+  std::cout << "computed determinant " << det << ", in " << ts << " ms"
+            << std::endl;
 
   std::free(mat);
 
